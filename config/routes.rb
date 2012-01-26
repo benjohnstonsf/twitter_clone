2686ef2e03443ff83978c27862424ebb82212dbe
Ben Johnston <benjohnstonsf@gmail.com>
2012-01-26 10:50:07 -0800

    Got rid of gets in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 TwitterClone::Application.routes.draw do
-  get "users/new"
-
-  get "pages/home"
-  
-  get "pages/contact"
-  
-  get "pages/about"
-  
-  get "pages/help"
+  # get "users/new"
+  # 
+  #  get "pages/home"
+  #  
+  #  get "pages/contact"
+  #  
+  #  get "pages/about"
+  #  
+  #  get "pages/help"
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
